43a48f0830b8e7a383c8614ceea7b35e8259862e
Dave Aitken <dave.aitken@gmail.com>
2024-06-12 15:45:34 +0100

    cleanup, split into an api, add various options

--- lua/direnv-nvim.lua
+++ lua/direnv-nvim.lua
@@ -1,53 +1,72 @@
 local M = {}
-M.setup = function()
-	vim.api.nvim_create_autocmd("BufEnter", {
-		pattern = "*",
-		callback = function(ev)
-			local cmd = { "direnv", "export", "json" }
-			local buf = vim.api.nvim_buf_get_name(0)
-			local bufdir = vim.fs.dirname(buf)
-			local result = vim.system(cmd, { text = true, cwd = bufdir }):wait()
-			local display_msg = result.stderr
-			print(display_msg)
-			if result.stdout == "" then
-				print("in sync already")
+
+local get_cwd = function()
+	local buf = vim.api.nvim_buf_get_name(0)
+	return vim.fs.dirname(buf)
+end
+
+M.status = function()
+	local status_result = vim.system({ "direnv", "status", "--json" }, { text = true, cwd = get_cwd() }):wait()
+	local status = vim.json.decode(status_result.stdout)
+	-- print(status_result.stdout)
+	-- print(vim.inspect(status))
+	if status.state.loadedRC == vim.NIL then
+		vim.cmd("redraw")
+		vim.notify("direnv: environment clear")
+	else
+		vim.cmd("redraw")
+		vim.notify("direnv: environment from: " .. status.state.loadedRC.path)
+	end
+end
+
+M.hook = function(opts)
+	local export_result = vim.system({ "direnv", "export", "json" }, { text = true, cwd = get_cwd() }):wait()
+	if export_result.stdout ~= "" then
+		for k, v in pairs(vim.json.decode(export_result.stdout)) do
+			if v == vim.NIL then
+				vim.env[k] = nil
 			else
-				for k, v in pairs(vim.json.decode(result.stdout)) do
-					if vim.startswith(v, "-") then
-						vim.env[k] = nil
-					else
-						vim.env[k] = v
-					end
-				end
+				vim.env[k] = v
 			end
+		end
+
+		if opts.hook.msg == "diff" then
+			local display_msg = export_result.stderr
+			if display_msg ~= nil then
+				local lines = vim.split(display_msg, "\n", { trimempty = true })
+				local diff_msg = lines[#lines]
+				if string.len(diff_msg) > vim.o.columns then
+					diff_msg = string.sub(diff_msg, 1, vim.o.columns - 20) .. "..."
+				end
+				vim.cmd("redraw")
+				vim.notify(diff_msg, vim.log.levels.INFO)
+			end
+		elseif opts.hook.msg == "status" then
+			M.status()
+		end
+	end
+end
+
+local default_opts = {
+	setup = {
+		autocmd_event = "FileType",
+		autocmd_pattern = "*",
+	},
+	hook = {
+		msg = "status", -- "diff" | "status",
+	},
+}
+
+M.setup = function(opts)
+	if opts == nil then
+		opts = default_opts
+	end
+	vim.api.nvim_create_autocmd(opts.setup.autocmd_event, {
+		pattern = opts.setup.autocmd_pattern,
+		callback = function()
+			M.hook(opts)
 		end,
 	})
-	-- local exports = vim.split(result.stdout, ";")
-	-- print("exports22")
-	-- print(vim.inspect(exports))
-	-- local kvs = {}
-	-- for _, export in pairs(exports) do
-	-- 	if export ~= "" then
-	-- 		local body = string.sub(export, 8)
-	-- 		local kv = vim.split(body, "=")
-	-- 		local k = kv[1]
-	-- 		local v = kv[2]
-	-- 		print(v)
-	-- 		if vim.startswith(v, "$'-") then
-	-- 			kvs[k] = { "remove", string.sub(v, 4, string.len(v)) }
-	-- 		elseif vim.startswith(v, "$'") then
-	-- 			kvs[k] = { "add", string.sub(v, 3, string.len(v)) }
-	-- 		end
-	-- 	end
-	-- end
-	--
-	-- print(vim.inspect(kvs))
-	-- for k, v in pairs(kvs) do
-	-- 	if v[1] == "add" then
-	-- 		vim.env[k] = v[2]
-	-- 	elseif v[1] == "remove" then
-	-- 		vim.env[k] = nil
-	-- 	end
-	-- end
 end
+
 return M
